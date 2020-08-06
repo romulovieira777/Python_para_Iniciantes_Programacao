bb3415b5ff99c906c62fc1cdc412db70178baf2a
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-08-06 16:22:18 -0300

    Variáveis

---
+++ variables.py
@@ -0,0 +1,9 @@
+personagem_nome = 'Rômulo'
+personagem_idade = '19'
+personagem_esporte = 'Skate'
+personagem_jogo = 'Futebol'
+
+print('Meu nome é ' + personagem_nome + ',')
+print('eu tenho ' + personagem_idade + '.')
+print('Meu esporte favorito é o ' + personagem_esporte + ',')
+print('eu gosto de jogar ' + personagem_jogo + ' nas minhas horas vagas.')
