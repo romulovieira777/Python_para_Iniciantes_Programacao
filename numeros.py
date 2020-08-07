73d9ebfe84f65a086d0dfce8b1b481bb3aaa9c07
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-08-07 17:17:23 -0300

    Números

---
+++ numeros.py
@@ -0,0 +1,17 @@
+print(6)
+print(-8)
+print(8.290)
+print(4 + 2)
+print(4 + 2.345)
+print(2 * 2 + 5)
+print(2 * (2 + 15))
+
+num1 = 5
+num2 = 10
+print(num1 + num2)
+print(num1 - num2)
+print(str(num1) + ' é o meu número da sorte.')
+print(min(2, 8))
+print(max(2, 8))
+print(pow(2, 3))
+print(round(11.7))
