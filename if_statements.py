e2c0df1f65019b30b0adfd36dc1ecf8955a1534d
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-08-10 15:18:02 -0300

    IF

---
+++ if_statements.py
@@ -0,0 +1,8 @@
+acordei_antes_9 = True
+fome = True
+if acordei_antes_9 and fome:
+    print('Hora de fazer o café da manhã!')
+elif acordei_antes_9 and not fome:
+    print('Hora de fazer uma corrida!')
+else:
+    print('Hora de fazer o almoço!')
